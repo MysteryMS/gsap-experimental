a964f3697542437396d7f669d9c126f1a64a4615
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2022-08-23 18:28:55 -0300

    atualização das animações

--- src/App.js
+++ src/App.js
@@ -2,44 +2,88 @@ import './App.css';
 import { gsap } from 'gsap';
 import { useEffect, useRef, useState } from 'react';
 
+const imgStyle = {
+  width: "200px",
+  height: "200px",
+  objectFit: "cover",
+  display: "none",
+  opacity: "0.0",
+}
+
 function App() {
-  const boxRef = useRef();
-  const divRef = useRef();
+  const tl = useRef();
+  const tl2 = useRef();
+  const tl3 = useRef();
+  const parent = useRef();
+  const q = gsap.utils.selector(parent)
+  const [audio] = useState(new Audio('https://cdn.discordapp.com/attachments/909079509997879337/1010586586859184250/she_-_Electric_Girl_HQ_NG7IYLByWCI.m4a'))
+  const [showText, setShowText] = useState(false);
+  const [glitch, setGlitch] = useState(false);
 
-  const [count, setCount] = useState(0);
-
-  // arrow function
   useEffect(() => {
-    gsap.to(boxRef.current, { rotation: "+=360", x: "-=300" })
-    // rotation = rotation + 360 => rotation += 360
+    audio.autoplay = true
+
+    setTimeout(() => {
+      setShowText(true);
+      console.log("showing text")
+    }, 26000)
+
+    setTimeout(() => {
+      setGlitch(true);
+      console.log("glitching")
+    }, 11800)
+
+    tl.current = gsap.timeline()
+      .to(q('#i'), { display: "block" })
+      .to(q('#i'), { opacity: "1.0", duration: 0.50, stagger: 0.55 })
+      .to(q('#i'), { opacity: "0.0", duration: 0.50, stagger: 0.55 })
+      .to(q('#i'), { display: "none" })
+      .repeat(1)
+      .delay(2)
+
+
   }, [])
 
-
   useEffect(() => {
-    gsap.to(boxRef.current, { fontSize: "50px" })
-  }, [count])
-
-  useEffect(() => {
-    if (count > 1) {
-      gsap.to(divRef.current, { height: `+=${count * 50}px` })
+    if (showText) {
+      tl2.current = gsap.timeline()
+        .to(q('#t'), { opacity: "1.0", duration: 0.50, stagger: 0.20 })
+        .to(q('#t'), { opacity: "0.0", duration: 0.50, stagger: 0.10 })
     }
-  }, [count])
+  }, [showText])
+
+  useEffect(() => {
+    if (glitch) {
+      tl3.current = new gsap.timeline()
+        .to(q('body'), { backgroundColor: "white", duration: 0.10 })
+        .to(q('body'), { backgroundColor: "black", duration: 0.10 })
+        .repeat(1)
+
+    }
+  })
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p ref={boxRef}>
-          Edit src/App.js and bom dia
-        </p>
+    <div className="App" ref={parent}>
+      <body>
+        <div id='parent' style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+          <img id='i' src="/img0.jpg" alt="img0" style={{ ...imgStyle, margin: "0 45px 59px 0" }} />
+          <img id='i' src="/img1.jpg" alt="img1" style={{ ...imgStyle, margin: "40px 72px 100px 89px" }} />
+          <img id='i' src="/img2.jpg" alt="img2" style={{ ...imgStyle, margin: "74px 41px 87px 90px" }} />
+          <img id='i' src="/img3.jpg" alt="img3" style={{ ...imgStyle, margin: "94px 77px 75px 136px" }} />
+          <img id='i' src="/img4.jpg" alt="img4" style={{ ...imgStyle, margin: "63px 41px 0 77px" }} />
+          <img id='i' src="/img5.jpg" alt="img5" style={{ ...imgStyle, margin: "97px 74px 0 78px" }} />
 
-        <button onClick={() => { setCount(count + 1) }}>
-          <p>mudar contador</p>
-        </button>
-
-        <div style={{ height: "100px", width: "50px", backgroundColor: "blue" }} ref={divRef}>
-
+          <div style={{ display: "flex", width: "100%", height: "100%", alignItems: "center", justifyContent: "center", }}>
+            <p id='t'>I'll </p>
+            <p id='t'>remind </p>
+            <p id='t'>you </p>
+            <p id='t'>how </p>
+            <p id='t'>it </p>
+            <p id='t'>feels </p>
+            <p id='t'>like... </p>
+          </div>
         </div>
-      </header>
+      </body>
     </div>
   );
 }
